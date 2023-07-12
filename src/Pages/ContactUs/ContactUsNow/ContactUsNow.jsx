59f34401c58ab70e1426d3d4ff35d68561b6bc6c
rezwanrahimr <rezwanrahim69@gmail.com>
2023-07-12 15:01:46 +0600

    contact us done

--- src/Pages/ContactUs/ContactUsNow/ContactUsNow.jsx
+++ src/Pages/ContactUs/ContactUsNow/ContactUsNow.jsx
@@ -6,16 +6,37 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./ContactUsNow.scss";
+import { useContext } from "react";
+import { AuthContexts } from "../../../Context/AuthContext";
+import Swal from "sweetalert2";
 
 const ContactUsNow = () => {
+  const { user } = useContext(AuthContexts);
+
+  //
+  const handleContact = (e) => {
+    e.preventDefault();
+    /*    const email = user?.email;
+    const name = user?.name;
+    const text = e.target.text.value; */
+
+    Swal.fire(
+      "Thanks !",
+      "we are contacting you as soon as possible !",
+      "success"
+    );
+    e.target.reset();
+  };
   return (
     <div className="contact-container p-12 ">
       <div className="md:flex items-center">
         <div className="contact-text-content md:w-1/2 p-3">
           <h2 className="text-secondary">CONTACT NOW</h2>
           <h1 className="text-primary">Have Question? Write a Message</h1>
-          <form action="">
+          <form onSubmit={handleContact}>
             <input
+              readOnly
+              value={user?.email}
               className="contact-input w-full"
               type="text"
               name="email"
@@ -23,6 +44,8 @@ const ContactUsNow = () => {
             />{" "}
             <br />
             <input
+              required
+              value={user?.name}
               className="contact-input w-full"
               type="text"
               name="name"
@@ -30,6 +53,7 @@ const ContactUsNow = () => {
             />{" "}
             <br />
             <textarea
+              required
               className="contact-text-area w-full"
               name="message"
               placeholder="Your message"
@@ -38,7 +62,10 @@ const ContactUsNow = () => {
               rows="10"
             ></textarea>{" "}
             <br />
-            <button className="btn btn-secondary w-full text-white">
+            <button
+              type="submit"
+              className="btn btn-secondary w-full text-white"
+            >
               send{" "}
               <FontAwesomeIcon
                 icon={faPaperPlane}
@@ -50,16 +77,18 @@ const ContactUsNow = () => {
         <div className="md:w-1/2 p-3">
           <div className="info-card-one info-card">
             <h1 className="text-primary font-bold text-xl">
-              <FontAwesomeIcon icon={faPhone} /> Phone:{" "}
+              <FontAwesomeIcon icon={faPhone} className="mx-2" /> Phone:{" "}
             </h1>
-            <h2>Assistance hours: Monday – Friday, 9 am to 5 pm</h2>
+            <h2 className="mt-2">
+              Assistance hours: Monday – Friday, 9 am to 5 pm
+            </h2>
             <h3 className="font-medium">(+91) 0000 000 000</h3>
           </div>
           <div className="info-card-two info-card">
             <h1 className="text-primary font-bold text-xl">
-              <FontAwesomeIcon icon={faEnvelope} /> Email:{" "}
+              <FontAwesomeIcon icon={faEnvelope} className="mx-2" /> Email:{" "}
             </h1>
-            <h2>
+            <h2 className="mt-2">
               Our support team will get back to in 24-h during standard business
               hours.
             </h2>
@@ -67,9 +96,11 @@ const ContactUsNow = () => {
           </div>
           <div className="info-card-three info-card">
             <h1 className="text-primary font-bold text-xl">
-              <FontAwesomeIcon icon={faComment} /> Skype
+              <FontAwesomeIcon icon={faComment} className="mx-2" /> Skype
             </h1>
-            <h2>We Are Online: Monday – Friday, 9 am to 5 pm</h2>
+            <h2 className="mt-2">
+              We Are Online: Monday – Friday, 9 am to 5 pm
+            </h2>
             <h3 className="font-medium">Creative-Agency</h3>
           </div>
         </div>
